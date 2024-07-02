1b82f587c8f7ac3346593f8aae939fed77181854
Anastasiia <nastyazdebska@gmail.com>
2024-07-02 18:17:57 +0300

    added to models/users new methods

--- src/models/users.py
+++ src/models/users.py
@@ -1,4 +1,7 @@
 class Users:
+    ROLE_USER = 0
+    ROLE_ADMIN = 1
+
     def __init__(self, id: int, username: str, email: str, avatar_filename: str,
                  password_hash: str, role: int, refresh_token: str):
         self.id = id
@@ -8,3 +11,11 @@ class Users:
         self.password_hash = password_hash
         self.role = role
         self.refresh_token = refresh_token
+
+    def __repr__(self):
+        return (f"User {self.id}, username {self.username}, email {self.email}, "
+                f"avatar_filename {self.avatar_filename}, password_hash {self.password_hash}, "
+                f"role {self.role}, refresh_token {self.refresh_token}")
+
+    def __str__(self):
+        return f"User(username={self.username}, email={self.email})"
