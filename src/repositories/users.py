4e3db54af144ea190df2bb7cf8296aaf92c6d3b3
Anastasiia <nastyazdebska@gmail.com>
2024-07-03 15:37:49 +0300

    added signin, signup in models/users and updated repositories/users

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,22 +1,31 @@
 from typing import Optional, List
 from UReader.src.models.users import Users
+from abc import abstractmethod
+from UReader.src.schemas.users import UserCreate, UserUpdate
 
 
 class UserInterface:
+
+    @abstractmethod
     def get_user_by_id(self, user_id: int) -> Optional[Users]:
         raise NotImplementedError
 
+    @abstractmethod
     def get_user_by_email(self, email: str) -> Optional[Users]:
         raise NotImplementedError
 
+    @abstractmethod
     def get_users(self, skip: int = 0, limit: int = 10) -> List[Users]:
         raise NotImplementedError
 
-    def create_user(self, user: Users) -> Users:
+    @abstractmethod
+    def create_user(self, user: UserCreate) -> Users:
         raise NotImplementedError
 
-    def update_user(self, user_id: int, user: Users) -> Users:
+    @abstractmethod
+    def update_user(self, user_id: int, user: UserUpdate) -> Users:
         raise NotImplementedError
 
+    @abstractmethod
     def delete_user(self, user_id: int) -> Users:
         raise NotImplementedError
