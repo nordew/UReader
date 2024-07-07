135f3a3872b231efb1dc7de02da6058bee08e3a4
nordew <horishnyi@proton.me>
2024-07-07 23:29:44 +0300

    fix: schemas namings; DAO methods;

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,31 +1,31 @@
 from typing import Optional, List
 from UReader.src.models.users import Users
 from abc import abstractmethod
-from UReader.src.schemas.users import UserCreate, UserUpdate
+from UReader.src.schemas.users import UserSchema, UserSchemaCreate, UserSchemaUpdate
 
 
 class UserInterface:
 
     @abstractmethod
-    def get_user_by_id(self, user_id: int) -> Optional[Users]:
+    def get_user_by_id(self, user_id: int) -> Optional[UserSchema]:
         raise NotImplementedError
 
     @abstractmethod
-    def get_user_by_email(self, email: str) -> Optional[Users]:
+    def get_user_by_email(self, email: str) -> Optional[UserSchema]:
         raise NotImplementedError
 
     @abstractmethod
-    def get_users(self, skip: int = 0, limit: int = 10) -> List[Users]:
+    def get_users(self, skip: int = 0, limit: int = 10) -> List[UserSchema]:
         raise NotImplementedError
 
     @abstractmethod
-    def create_user(self, user: UserCreate) -> Users:
+    def create_user(self, user: Users) -> None:
         raise NotImplementedError
 
     @abstractmethod
-    def update_user(self, user_id: int, user: UserUpdate) -> Users:
+    def update_user(self, user_id: int, user: Users) -> None:
         raise NotImplementedError
 
     @abstractmethod
-    def delete_user(self, user_id: int) -> Users:
+    def delete_user(self, user_id: int) -> None:
         raise NotImplementedError
